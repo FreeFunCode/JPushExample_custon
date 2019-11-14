da50b5577561ad96fbe39ba6ce30b1b0d00198cc
huangguangzhi <940286175@qq.com>
2019-11-14 11:16:41 +0800

    “JpushExample初始化”

---
+++ build/generated/source/buildConfig/debug/com/jpush/test05/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.jpush.test05;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.jpush.test05";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 338;
+  public static final String VERSION_NAME = "3.3.8";
+}
---
+++ src/com/example/jpushdemo/MyReceiver.java
@@ -0,0 +1,224 @@
+package com.example.jpushdemo;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.NotificationCompat;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.jpush.test05.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+
+import cn.jpush.android.api.JPushInterface;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
+
+/**
+ * 自定义接收器
+ * <p>
+ * 如果不定义这个 Receiver，则：
+ * 1) 默认用户会打开主界面
+ * 2) 接收不到自定义消息
+ */
+public class MyReceiver extends BroadcastReceiver {
+    private static final String TAG = "MyReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        try {
+            Bundle bundle = intent.getExtras();
+            Logger.d(TAG, "[MyReceiver] onReceive - " + intent.getAction() + ", extras: " + printBundle(bundle));
+
+            if (JPushInterface.ACTION_REGISTRATION_ID.equals(intent.getAction())) {
+                String regId = bundle.getString(JPushInterface.EXTRA_REGISTRATION_ID);
+                Logger.d(TAG, "[MyReceiver] 接收Registration Id : " + regId);
+                //send the Registration Id to your server...
+
+            } else if (JPushInterface.ACTION_MESSAGE_RECEIVED.equals(intent.getAction())) {
+                Logger.d(TAG, "[MyReceiver] 接收到推送下来的自定义消息: " + bundle.getString(JPushInterface.EXTRA_MESSAGE));
+                processCustomMessage(context, bundle);
+
+            } else if (JPushInterface.ACTION_NOTIFICATION_RECEIVED.equals(intent.getAction())) {
+                Logger.d(TAG, "[MyReceiver] 接收到推送下来的通知");
+                int notifactionId = bundle.getInt(JPushInterface.EXTRA_NOTIFICATION_ID);
+                Logger.d(TAG, "[MyReceiver] 接收到推送下来的通知的ID: " + notifactionId);
+
+            } else if (JPushInterface.ACTION_NOTIFICATION_OPENED.equals(intent.getAction())) {
+                Logger.d(TAG, "[MyReceiver] 用户点击打开了通知");
+
+                //打开自定义的Activity
+                Intent i = new Intent(context, TestActivity.class);
+                i.putExtras(bundle);
+                //i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                context.startActivity(i);
+
+            } else if (JPushInterface.ACTION_RICHPUSH_CALLBACK.equals(intent.getAction())) {
+                Logger.d(TAG, "[MyReceiver] 用户收到到RICH PUSH CALLBACK: " + bundle.getString(JPushInterface.EXTRA_EXTRA));
+                //在这里根据 JPushInterface.EXTRA_EXTRA 的内容处理代码，比如打开新的Activity， 打开一个网页等..
+
+            } else if (JPushInterface.ACTION_CONNECTION_CHANGE.equals(intent.getAction())) {
+                boolean connected = intent.getBooleanExtra(JPushInterface.EXTRA_CONNECTION_CHANGE, false);
+                Logger.w(TAG, "[MyReceiver]" + intent.getAction() + " connected state change to " + connected);
+            } else {
+                Logger.d(TAG, "[MyReceiver] Unhandled intent - " + intent.getAction());
+            }
+        } catch (Exception e) {
+
+        }
+
+    }
+
+    // 打印所有的 intent extra 数据
+    private static String printBundle(Bundle bundle) {
+        StringBuilder sb = new StringBuilder();
+        for (String key : bundle.keySet()) {
+            if (key.equals(JPushInterface.EXTRA_NOTIFICATION_ID)) {
+                sb.append("\nkey:" + key + ", value:" + bundle.getInt(key));
+            } else if (key.equals(JPushInterface.EXTRA_CONNECTION_CHANGE)) {
+                sb.append("\nkey:" + key + ", value:" + bundle.getBoolean(key));
+            } else if (key.equals(JPushInterface.EXTRA_EXTRA)) {
+                if (TextUtils.isEmpty(bundle.getString(JPushInterface.EXTRA_EXTRA))) {
+                    Logger.i(TAG, "This message has no Extra data");
+                    continue;
+                }
+
+                try {
+                    JSONObject json = new JSONObject(bundle.getString(JPushInterface.EXTRA_EXTRA));
+                    Iterator<String> it = json.keys();
+
+                    while (it.hasNext()) {
+                        String myKey = it.next();
+                        sb.append("\nkey:" + key + ", value: [" +
+                                myKey + " - " + json.optString(myKey) + "]");
+                    }
+                } catch (JSONException e) {
+                    Logger.e(TAG, "Get message extra JSON error!");
+                }
+
+            } else {
+                sb.append("\nkey:" + key + ", value:" + bundle.get(key));
+            }
+        }
+        return sb.toString();
+    }
+
+
+    /**
+     * 实现自定义推送声音
+     *
+     * @param context
+     * @param bundle
+     */
+
+    private void processCustomMessage(Context context, Bundle bundle) {
+
+
+        // 此处必须兼容android O设备，否则系统版本在O以上可能不展示通知栏
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            NotificationChannel channel = new NotificationChannel(
+//                    context.getPackageName(),
+//                    TAG,
+//                    NotificationManager.IMPORTANCE_HIGH
+//
+//            );
+//            channel.getSound();
+//            channel.setSound(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + context.getPackageName() + "/" + R.raw.msg_sound),null);
+//            channel.getSound();
+//            notificationManager.createNotificationChannel(channel);
+//
+//        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            // 通知渠道的id 这个地方只要一直即可
+            String id = "111111";
+            // 用户可以看到的通知渠道的名字.
+            CharSequence name = "notification channel";
+            // 用户可以看到的通知渠道的描述
+            String description = "notification description";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(id, name, importance);
+            // 配置通知渠道的属性
+            mChannel.setDescription(description);
+            // 设置通知出现时的闪灯（如果 android 设备支持的话）
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            // 自定义声音
+            mChannel.setSound(Uri.parse("android.resource://" + context.getPackageName() + "/raw/msg_sound"),null);
+            // 设置通知出现时的震动（如果 android 设备支持的话）
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            //最后在notificationmanager中创建该通知渠道
+            mNotificationManager.createNotificationChannel(mChannel);
+        }
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(context,"111111");
+
+        notification.setAutoCancel(true)
+                .setContentText("自定义推送声音1")
+                .setContentTitle("极光测试1")
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setSound(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + context.getPackageName() + "/" + R.raw.msg_sound));
+
+        Intent mIntent = new Intent(context, TestActivity.class);
+
+        mIntent.putExtras(bundle);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, mIntent, 0);
+
+        notification.setContentIntent(pendingIntent);
+
+        // channelId非常重要，不设置通知栏不展示
+        notification.setChannelId("111111");
+
+        Notification notify = notification.build();
+
+        notify.sound = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + context.getPackageName() + "/" + R.raw.msg_sound);
+
+        notificationManager.notify(3, notify);
+
+
+    }
+
+    //send msg to MainActivity -- 官方自定义接受消息Example
+    private void processCustomMessage1(Context context, Bundle bundle) {
+        if (MainActivity.isForeground) {
+            String message = bundle.getString(JPushInterface.EXTRA_MESSAGE);
+            String extras = bundle.getString(JPushInterface.EXTRA_EXTRA);
+            Intent msgIntent = new Intent(MainActivity.MESSAGE_RECEIVED_ACTION);
+            msgIntent.putExtra(MainActivity.KEY_MESSAGE, message);
+            if (!ExampleUtil.isEmpty(extras)) {
+                try {
+                    JSONObject extraJson = new JSONObject(extras);
+                    if (extraJson.length() > 0) {
+                        msgIntent.putExtra(MainActivity.KEY_EXTRAS, extras);
+                    }
+                } catch (JSONException e) {
+
+                }
+
+            }
+
+            LocalBroadcastManager.getInstance(context).sendBroadcast(msgIntent);
+        }
+    }
+}
